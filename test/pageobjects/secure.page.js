f130a27a38d5403aba15422b9f1bfbfd982687d6
Muhammad Farooq Khalid <[farooqsheikh247@gmail.com]>
2022-01-09 18:54:31 +0500

    Added second TC with POM implementation

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -12,6 +12,8 @@ class SecurePage extends Page {
     get flashAlert() {
         return $('#flash');
     }
+
+    get btnLogout(){  return $('a[href="/logout"]') }
 }
 
 module.exports = new SecurePage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -10,6 +10,17 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+
+    it('Should logout and verify logout alert message', () => {
+        //const btnLogout = $('a[href="/logout"]')
+        //const flashAlert = $('#flash')
+
+        //btnLogout.click();
+        SecurePage.btnLogout.click();
+        expect(SecurePage.flashAlert).toHaveTextContaining("You logged out of the secure area!");
+        //SecurePage.btnLogout.click();
+        //expect(SecurePage.flashAlert).toHaveTextContaining("You logged out of the secure area!");
+    });
 });
 
 
