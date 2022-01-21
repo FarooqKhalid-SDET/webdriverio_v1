c74605123b1346e2f9c043f38c3eb819a379154d
Muhammad Farooq Khalid <[farooqsheikh247@gmail.com]>
2022-01-21 17:50:00 +0500

    Element State

--- test/specs/main.e2e.js
+++ test/specs/main.e2e.js
@@ -1,9 +1,35 @@
+const internetPage = require("../pageobjects/internet.page")
+
 describe('Main Page', () => {
     it('Verify List Items', ()=>{
         browser.url('/');
-
-        const listExamples = $$("ul li");
-        console.log(listExamples)
-        expect(listExamples).toBeElementsArrayOfSize({gte: 1});
+        browser.maximizeWindow();
+        
+        internetPage.getLiText();
+        //internetPage.getSpecificElementText(3);
     });
+
+    it("Is Footer Displayed", ()=>{
+        console.log(internetPage.pageFooter.isDisplayed());
+    })
+
+    it("Does the Header Exist", ()=> {
+        console.log(internetPage.pageHeader.isExisting());
+    })
+
+    it("Is Footer in viewport?", ()=>{
+        console.log(internetPage.pageFooter.isDisplayedInViewport());
+    })
+
+    it("Is Header in viewport?", ()=>{
+        console.log(internetPage.pageHeader.isDisplayedInViewport());
+    })
+
+    it("Is Sub-Header is Enabled?", ()=>{
+        console.log(internetPage.subHeading.isEnabled());
+    })
+
+    it("Click Element", ()=>{
+        internetPage.clickOnLink();
+    })
 });
